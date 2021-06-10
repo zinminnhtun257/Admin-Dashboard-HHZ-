04a24fd489cd6cbe149457f8d9704d22bb901682
zinminn <zinminnhtun257@gmail.com>
2021-06-10 19:22:44 +0630

    change to chart js old version

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,79 +1,86 @@
-$(".counter-up").counterUp(
-    {
-        delay: 10,
-        time :1000
-    }
-);
+$('.counter-up').counterUp({
+    delay: 10,
+    time: 1000
+});
 
-let dateArr = ["july 21","july 20","july 19","july 18","july 17","july 16","july 15","july 14","july 13","july 12","july 11"];
-let orderCountArr =[7,5,6,4,6,4,8,6,8,9,6];
+let dateArr = ['jul 21','jul 20','jul 19','jul 18','jul 17','jul 16','jul 15','jul 14','jul 13','jul 12','jul 11'];
+let orderCountArr = [7, 5, 6, 4, 6, 4,8,6,8,9,6];
 let viewerCountArr = [13,12,15,14,20,17,19,16,23,33,16];
 
 let ov = document.getElementById('ov').getContext('2d');
-        let ovChart = new Chart(ov, {
-            type: 'line',
-            data: {
-                labels: dateArr,
-                datasets: [
-                    {
-                    label: 'Order Count',
-                    data: orderCountArr,
-                    backgroundColor: [
-                        '#007bff40'
-                    ],
-                    borderColor: [
-                        '#007bff'
-                    ],
-                    borderWidth: 1,
-                    lineTension : 0,
-                    fill: true,
-                },
+let ovChart = new Chart(ov, {
+    type: 'line',
+    data: {
+        labels: dateArr,
+        datasets: [
+            {
+                label: 'Orders Count',
+                data: orderCountArr,
+                backgroundColor: [
+                    '#007bff30',
+                ],
+                borderColor: [
+                    '#007bff',
+                ],
+                borderWidth: 1,
+                tension:0
+            },
+            {
+                label: 'Viewer Count',
+                data: viewerCountArr,
+                backgroundColor: [
+                    '#28a74530',
+                ],
+                borderColor: [
+                    '#28a745',
+                ],
+                borderWidth: 1,
+                tension:0
+            }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                display:false,
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            xAxes:[
                 {
-                    label: 'Viewer Count',
-                    data: viewerCountArr,
-                    backgroundColor: [
-                        '#28a74530'
-                    ],
-                    borderColor: [
-                        '#28a745'
-                    ],
-                    borderWidth: 1,
-                    lineTension : 0,
-                    fill: true
+                    display:false,
+                    gridLines:[
+                        {
+                            display:false
+                        }
+                    ]
                 }
             ]
-            },
-            options: {
-                scales: {
-                    yAxes: {
-                        display:false,
-                        beginAtZero: true
-                    },
-                    xAxes: {   
-                        display:false,
-                            
-                    }
-                },
-                legend:{
-                    display: false,
-                    position: 'bottom',
-                    label: {
-                        fontColor:"#333",
-                        usePointStyle:true 
-                    }
-                }
+        },
+        legend:{
+            display: true,
+            shape:"circle",
+            position: 'top',
+            labels: {
+                fontColor: '#333',
+                usePointStyle:true
             }
-        });
-        let orderFromPlace = [5,15,4,9,7];
-        let places = ["YNG","MDY","NPD","Shan","MGW"];
+        }
+    }
+});
+
+let orderFromPlace = [5,15,4,9,7];
+let places = ["YGN","MDY","NPY","SHAN","MGW"];
+
 let op = document.getElementById('op').getContext('2d');
 let opChart = new Chart(op, {
     type: 'doughnut',
     data: {
-        labels: places,
+        labels:places,
         datasets: [{
             label: '# of Votes',
-            data: orderFromPlace,
+            data:orderFromPlace,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -94,18 +101,26 @@ let opChart = new Chart(op, {
         }]
     },
     options: {
-        legend: {
-            position: "bottom",
-        },
         scales: {
-            y: {
-                display: false,
-                beginAtZero: true
-            },
-            x: {
-                display: false,
-            }
+            yAxes: [{
+                display:false,
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [
+                {
+                    display:false
+                }
+            ]
         },
-
+        legend:{
+            display: true,
+            position: 'bottom',
+            labels: {
+                fontColor: '#333',
+                usePointStyle:true
+            }
+        }
     }
 });
